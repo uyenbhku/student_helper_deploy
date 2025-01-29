9c1aec29135e6694c99c9467570bc1e5fd3e9ead
uyenbhku <uyen.bhku@gmail.com>
2025-01-29 18:17:12 +0700

    features/fast pasting from excel and UIT, mobile responsive

--- static/js/components/table/EditableCell.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.EditableCell = void 0;
-var _react = _interopRequireDefault(require("react"));
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-var EditableCell = exports.EditableCell = function EditableCell() {};
--- static/js/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _react = _interopRequireDefault(require("react"));
-var _client = require("react-dom/client");
-var _App = _interopRequireDefault(require("./App"));
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-var container = document.getElementById('root');
-var root = (0, _client.createRoot)(container); // createRoot(container!) if you use TypeScript
-root.render(/*#__PURE__*/_react["default"].createElement(_App["default"], null));
